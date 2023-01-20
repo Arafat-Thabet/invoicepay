3b46a876043c91c3038be0406b8e81e350659d79
arafat_thabet <arafat.733011506@gmail.com>
2023-01-20 23:30:02 +0300

    update

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -25,7 +25,7 @@
             // 10 KWD = 10 * 1000 Fils
             // 10 JPY = 10 JPY (Japanese Yen does not have fractions)
             amount: <?= $_GET['total'] ?>,
-            currency: "<?= getSetting('currency') ?>",
+            currency: "SAR",
             description: 'Coffee Order #1',
             publishable_api_key: 'pk_test_AQpxBV31a29qhkhUYFYUFjhwllaDVrxSq5ydVNui',
             callback_url: 'https://moyasar.com/thanks',
